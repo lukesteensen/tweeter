1c80cee853c6821709b85343b3655a6ac3638296
Luke Steensen <luke.steensen@gmail.com>
2012-04-22 22:22:53 -0500

    make links work better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def page(str)
+    case str
+    when :home
+      params[:controller] == 'posts' and params[:action] == 'index'
+    when :new_post
+      params[:controller] == 'posts' and params[:action] == 'new'
+    when :users
+      params[:controller] == 'users' and params[:action] == 'index'
+    end
+  end
 end
